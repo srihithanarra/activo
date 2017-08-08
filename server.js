54c04e3724a2c4aa32df2a01c21362cd0764b172
Srihitha <srihitha.narra@prodigi.io>
2017-08-08 17:02:18 +0530

    Removed file loader for now

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ var app = express();
 var DIST_PATH = path.join(__dirname, "dist");
 
 app.use(express.static(DIST_PATH));
+app.listen(PORT);
 
 //Sample home route
-app.get("/home", function (req, res) {
-    res.sendFile( path.join(DIST_PATH, "index.html"));
+app.get("/events", function (req, res) {
+  res.json({
+      events: [
+      {
+          'day' : 1,
+         'title': 'Learning Tuesdays',
+         'start_time' : '11:00AM',
+         'end_time' : '12:00AM',
+     },
+     {
+         'day' : 2,
+         'title': 'Fun Wednesday',
+         'start_time' : '4:00PM',
+         'end_time' : '5:00PM',
+     },
+  ]});
 })
-
-app.listen(PORT);
